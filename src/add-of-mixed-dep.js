bf7178aefe171ee53825fbf35c9121e2d62b34fa
迷渡 <justjavac@gmail.com>
2017-02-07 15:56:15 +0800

    Create add-of-mixed-dep.js

---
+++ src/add-of-mixed-dep.js
@@ -0,0 +1,14 @@
+// flags: --trace-opt --trace-deopt
+
+function add(obj) {
+    return obj.prop + obj.prop;
+}
+
+var length = 10000;
+var i = 0;
+var a = { prop: 'a' };
+var b = { prop: 1 };
+
+for (i = 0; i < length; i++) {
+    add(i !== 8000 ? b : b);
+}
